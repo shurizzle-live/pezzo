f20792723006f2bfbbc70c1dd5541ef85a3b0dd3
shurizzle <me@shurizzle.dev>
2023-03-25 23:10:04 +0100

    Parse cli arguments and find command bin

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,42 @@
+use std::{ffi::OsString, os::unix::process::CommandExt};
+
+use clap::Parser;
 use pezzo::unix::Context;
 
 extern crate pezzo;
 
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    #[arg(short, long, value_name = "USER")]
+    pub user: Option<OsString>,
+    #[arg(short, long, value_name = "GROUP")]
+    pub group: Option<OsString>,
+    #[arg(trailing_var_arg(true), required(true))]
+    pub command: Vec<OsString>,
+}
+
 fn main() {
+    let Cli {
+        user,
+        group,
+        command: mut args,
+    } = Cli::parse();
+
+    let command = args.remove(0);
+    let command = if let Ok(command) = which::which(&command) {
+        command
+    } else {
+        eprintln!("Command {:?} not found.", command);
+        std::process::exit(1);
+    };
+
     let ctx = Context::current().unwrap();
 
     ctx.escalate_permissions();
     ctx.check_file_permissions(ctx.exe());
 
-    // ctx.authenticate();
+    ctx.authenticate();
+
+    // std::process::Command::new(command).args(args).exec();
 }
