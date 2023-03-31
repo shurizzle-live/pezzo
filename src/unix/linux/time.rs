2e9e7debdee02558c163c8764082a80f39280ae8
shurizzle <me@shurizzle.dev>
2023-03-31 23:51:11 +0200

    Add support for time::now() for linux <2.6.39

--- src/unix/linux/time.rs
+++ src/unix/linux/time.rs
@@ -3,7 +3,14 @@ use std::mem::{self, MaybeUninit};
 pub fn now() -> u64 {
     unsafe {
         let mut time = MaybeUninit::<libc::timespec>::uninit();
-        libc::clock_gettime(libc::CLOCK_BOOTTIME, time.as_mut_ptr());
+        libc::clock_gettime(
+            if crate::version!(>= 2, 6, 39) {
+                libc::CLOCK_BOOTTIME
+            } else {
+                libc::CLOCK_MONOTONIC
+            },
+            time.as_mut_ptr(),
+        );
         let time = time.assume_init();
         mem::transmute(time.tv_sec)
     }
