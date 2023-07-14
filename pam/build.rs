6a830b5e367501b5cb214210c2262db5a8ac0dba
shurizzle <shura1991@gmail.com>
2023-07-14 10:01:42 +0200

    Add OpenPAM support, enabled only on Apple

--- pam/build.rs
+++ pam/build.rs
@@ -1,8 +1,7 @@
 use std::{
     cell::RefCell,
     collections::HashSet,
-    env,
-    fmt::{self},
+    env, fmt,
     fs::File,
     io::{Read, Write},
     path::{Path, PathBuf},
@@ -15,12 +14,12 @@ use bindgen::callbacks::{IntKind, ParseCallbacks};
 static LINUX_RETURN_VALUES: phf::Map<&'static str, (&'static str, &'static str, &'static str)> = phf::phf_map! {
     "PAM_SUCCESS" => ("Success", "Successful function return", "Success"),
     "PAM_OPEN_ERR" => ("Open", "dlopen() failure when dynamically loading a service module", "Failed to load module"),
-    "PAM_SYMBOL_ERR" => ("Symbol", "Symbol not found", "Symbol not found"),
-    "PAM_SERVICE_ERR" => ("Service", "Error in service module", "Error in service module"),
-    "PAM_SYSTEM_ERR" => ("System", "System error", "System error"),
-    "PAM_BUF_ERR" => ("Buffer", "Memory buffer error", "Memory buffer error"),
-    "PAM_PERM_DENIED" => ("PermissionDenied", "Permission denied", "Permission denied"),
-    "PAM_AUTH_ERR" => ("Authentication", "Authentication failure", "Authentication failure"),
+    "PAM_SYMBOL_ERR" => ("Symbol", "Symbol not found", ""),
+    "PAM_SERVICE_ERR" => ("Service", "Error in service module", ""),
+    "PAM_SYSTEM_ERR" => ("System", "System error", ""),
+    "PAM_BUF_ERR" => ("Buffer", "Memory buffer error", ""),
+    "PAM_PERM_DENIED" => ("PermissionDenied", "Permission denied", ""),
+    "PAM_AUTH_ERR" => ("Authentication", "Authentication failure", ""),
     "PAM_CRED_INSUFFICIENT" => (
         "InsufficientCredentials",
         "Can not access authentication data due to insufficient credentials",
@@ -31,11 +30,7 @@ static LINUX_RETURN_VALUES: phf::Map<&'static str, (&'static str, &'static str, 
         "Underlying authentication service can not retrieve authentication information",
         "Authentication service cannot retrieve authentication info"
     ),
-    "PAM_USER_UNKNOWN" => (
-        "UnknownUser",
-        "User not known to the underlying authentication module",
-        "User not known to the underlying authentication module"
-    ),
+    "PAM_USER_UNKNOWN" => ("UnknownUser", "User not known to the underlying authentication module", ""),
     "PAM_MAXTRIES" => (
         "MaximumRetries",
         "An authentication service has maintained a retry count which has been reached. No further retries should be attempted",
@@ -47,44 +42,20 @@ static LINUX_RETURN_VALUES: phf::Map<&'static str, (&'static str, &'static str, 
         "Authentication token is no longer valid; new one required"
     ),
     "PAM_ACCT_EXPIRED" => ("AccountExpired", "User account has expired", "User account has expired"),
-    "PAM_SESSION_ERR" => (
-        "Session",
-        "Can not make/remove an entry for the specified session",
-        "Cannot make/remove an entry for the specified session"
-    ),
+    "PAM_SESSION_ERR" => ("Session", "Cannot make/remove an entry for the specified session", ""),
     "PAM_CRED_UNAVAIL" => (
         "CredentialsUnavailable",
         "Underlying authentication service can not retrieve user credentials unavailable",
         "Authentication service cannot retrieve user credentials"
     ),
-    "PAM_CRED_EXPIRED" => ( "CredentialsExpired", "User credentials expired", "User credentials expired"),
-    "PAM_CRED_ERR" => ("Credentials", "Failure setting user credentials", "Failure setting user credentials"),
-    "PAM_NO_MODULE_DATA" => (
-        "NoModuleData",
-        "No module specific data is present",
-        "No module specific data is present"
-    ),
-    "PAM_CONV_ERR" => ("Conversation", "Conversation error", "Conversation error"),
-    "PAM_AUTHTOK_ERR" => (
-        "AuthenticationToken",
-        "Authentication token manipulation error",
-        "Authentication token manipulation error"
-    ),
-    "PAM_AUTHTOK_RECOVERY_ERR" => (
-        "AuthenticationRecovery",
-        "Authentication information cannot be recovered",
-        "Authentication information cannot be recovered"
-    ),
-    "PAM_AUTHTOK_LOCK_BUSY" => (
-        "AuthenticationLockBusy",
-        "Authentication token lock busy",
-        "Authentication token lock busy"
-    ),
-    "PAM_AUTHTOK_DISABLE_AGING" => (
-        "AuthenticationTokenAgingDisabled",
-        "Authentication token aging disabled",
-        "Authentication token aging disabled"
-    ),
+    "PAM_CRED_EXPIRED" => ("CredentialsExpired", "User credentials expired", ""),
+    "PAM_CRED_ERR" => ("Credentials", "Failure setting user credentials", ""),
+    "PAM_NO_MODULE_DATA" => ("NoModuleData", "No module specific data is present", ""),
+    "PAM_CONV_ERR" => ("Conversation", "Conversation error", ""),
+    "PAM_AUTHTOK_ERR" => ("AuthenticationToken", "Authentication token manipulation error", ""),
+    "PAM_AUTHTOK_RECOVERY_ERR" => ("AuthenticationRecovery", "Authentication information cannot be recovered", ""),
+    "PAM_AUTHTOK_LOCK_BUSY" => ("AuthenticationLockBusy", "Authentication token lock busy", ""),
+    "PAM_AUTHTOK_DISABLE_AGING" => ("AuthenticationTokenAgingDisabled", "Authentication token aging disabled", ""),
     "PAM_TRY_AGAIN" => (
         "TryAgain",
         "Preliminary check by password service",
@@ -101,8 +72,8 @@ static LINUX_RETURN_VALUES: phf::Map<&'static str, (&'static str, &'static str, 
         "user's authentication token has expired",
         "Authentication token expired"
     ),
-    "PAM_MODULE_UNKNOWN" => ( "ModuleUnknown", "module is not known", "Module is unknown"),
-    "PAM_BAD_ITEM" => ("BadItem","Bad item passed to pam_*_item()", "Bad item passed to pam_*_item()"),
+    "PAM_MODULE_UNKNOWN" => ("ModuleUnknown", "Module is not known", ""),
+    "PAM_BAD_ITEM" => ("BadItem", "Bad item passed to pam_*_item()", "Bad item passed to pam_*_item()"),
     "PAM_CONV_AGAIN" => (
         "ConversationAgain",
         "conversation function is event driven and data is not available yet",
@@ -113,6 +84,43 @@ static LINUX_RETURN_VALUES: phf::Map<&'static str, (&'static str, &'static str, 
         "please call this function again to complete authentication stack. Before calling again, verify that conversation is completed",
         "Application needs to call libpam again"
     ),
+};
+
+static OPEN_RETURN_VALUES: phf::Map<&'static str, (&'static str, &'static str, &'static str)> = phf::phf_map! {
+    "PAM_SUCCESS" => ("Success", "Success", ""),
+    "PAM_OPEN_ERR" => ("Open", "Failed to load module", ""),
+    "PAM_SYMBOL_ERR" => ("Symbol", "Invalid symbol", ""),
+    "PAM_SERVICE_ERR" => ("Service", "Error in service module", ""),
+    "PAM_SYSTEM_ERR" => ("System", "System error", ""),
+    "PAM_BUF_ERR" => ("Buffer", "Memory buffer error", ""),
+    "PAM_CONV_ERR" => ("Conversation", "Conversation failure", ""),
+    "PAM_PERM_DENIED" => ("PermissionDenied", "Permission denied", ""),
+    "PAM_MAXTRIES" => ("MaximumRetries", "Maximum number of tries exceeded", ""),
+    "PAM_AUTH_ERR" => ("Authentication", "Authentication error", ""),
+    "PAM_NEW_AUTHTOK_REQD" => ("NewAuthenticationTokenRequired", "New authentication token required", ""),
+    "PAM_CRED_INSUFFICIENT" => ("InsufficientCredentials", "Insufficient credentials", ""),
+    "PAM_AUTHINFO_UNAVAIL" => ("AuthenticationInfoUnavailable", "Authentication information is unavailable", ""),
+    "PAM_USER_UNKNOWN" => ("UnknownUser", "Unknown user", ""),
+    "PAM_CRED_UNAVAIL" => ("CredentialsUnavailable", "Failed to retrieve user credentials", ""),
+    "PAM_CRED_EXPIRED" => ("CredentialsExpired", "User credentials have expired", ""),
+    "PAM_CRED_ERR" => ("Credentials", "Failed to set user credentials", ""),
+    "PAM_ACCT_EXPIRED" => ("AccountExpired", "User account has expired", ""),
+    "PAM_AUTHTOK_EXPIRED" => ("AuthenticationTokenExpired", "Password has expired", ""),
+    "PAM_SESSION_ERR" => ("Session", "Session failure", ""),
+    "PAM_AUTHTOK_ERR" => ("AuthenticationToken", "Authentication token failure", ""),
+    "PAM_AUTHTOK_RECOVERY_ERR" => ("AuthenticationRecovery", "Failed to recover old authentication token", ""),
+    "PAM_AUTHTOK_LOCK_BUSY" => ("AuthenticationLockBusy", "Authentication token lock busy", ""),
+    "PAM_AUTHTOK_DISABLE_AGING" => ("AuthenticationTokenAgingDisabled", "Authentication token aging disabled", ""),
+    "PAM_NO_MODULE_DATA" => ("NoModuleData", "Module data not found", ""),
+    "PAM_IGNORE" => ("Ignore", "Ignore this module", ""),
+    "PAM_ABORT" => ("Abort", "General failure", ""),
+    "PAM_TRY_AGAIN" => ("TryAgain", "Try again", ""),
+    "PAM_MODULE_UNKNOWN" => ("ModuleUnknown", "Unknown module type", ""),
+    "PAM_DOMAIN_UNKNOWN" => ("DomainUnknown", "Unknown authentication domain", ""),
+    "PAM_BAD_HANDLE" => ("BadHandle", "Invalid PAM handle", ""),
+    "PAM_BAD_ITEM" => ("BadItem", "Unrecognized or restricted item", ""),
+    "PAM_BAD_FEATURE" => ("BadFeature", "Unrecognized or restricted feature", ""),
+    "PAM_BAD_CONSTANT" => ("PAM_BAD_CONSTANT", "Invalid constant", ""),
 };
 
 fn main() {
@@ -161,6 +169,8 @@ fn main_apple() {
 }
 
 fn linux_pam() {
+    println!("cargo:rustc-link-lib=pam");
+
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     #[derive(Debug)]
@@ -175,19 +185,15 @@ fn linux_pam() {
         }
     }
 
-    let bindings = {
-        let errors = Rc::new(RefCell::new(HashSet::new()));
-        let builder = bindgen::Builder::default()
-            .header_contents("wrapper.h", "#include <security/pam_appl.h>")
-            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-            .ctypes_prefix("libc")
-            .parse_callbacks(Box::new(ErrorsConstants(errors.clone())));
-
-        Bindings::linux(
-            builder.generate().expect("Unable to generate bindings"),
-            RefCell::take(&errors),
-        )
-    };
+    let errors = Rc::new(RefCell::new(HashSet::new()));
+    let builder = bindgen::Builder::default()
+        .header_contents("wrapper.h", "#include <security/pam_appl.h>")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .ctypes_prefix("libc")
+        .parse_callbacks(Box::new(ErrorsConstants(errors.clone())));
+    let bindings = builder.generate().expect("Unable to generate bindings");
+    let errors = RefCell::take(&errors);
+    let bindings = Bindings::linux(&bindings, &errors);
 
     let mut f = File::create(out_path.join("pam.rs")).unwrap();
     let formatter = Formatter::new();
@@ -195,26 +201,84 @@ fn linux_pam() {
 }
 
 fn open_pam() {
-    unimplemented!()
+    println!("cargo:rustc-link-lib=pam");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    #[derive(Debug)]
+    struct ErrorsConstants(Rc<RefCell<HashSet<String>>>);
+
+    impl ParseCallbacks for ErrorsConstants {
+        fn enum_variant_name(
+            &self,
+            _enum_name: Option<&str>,
+            name: &str,
+            _variant_value: bindgen::callbacks::EnumVariantValue,
+        ) -> Option<String> {
+            let is_return = if OPEN_RETURN_VALUES.contains_key(name) {
+                self.0.borrow_mut().insert(name.to_string());
+                true
+            } else {
+                false
+            };
+
+            if is_return
+                || [
+                    "PAM_PROMPT_ECHO_OFF",
+                    "PAM_PROMPT_ECHO_ON",
+                    "PAM_TEXT_INFO",
+                    "PAM_ERROR_MSG",
+                ]
+                .contains(&name)
+            {
+                Some(format!("__ORIGINAL_{}", name))
+            } else {
+                None
+            }
+        }
+    }
+
+    let errors = Rc::new(RefCell::new(HashSet::new()));
+    let builder = bindgen::Builder::default()
+        .header_contents("wrapper.h", "#include <security/pam_appl.h>")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .ctypes_prefix("libc")
+        .parse_callbacks(Box::new(ErrorsConstants(errors.clone())));
+    let bindings = builder.generate().expect("unable to generate bindings");
+    let errors = RefCell::take(&errors);
+    let bindings = Bindings::open(&bindings, &errors);
+
+    let mut f = File::create(out_path.join("pam.rs")).unwrap();
+    let formatter = Formatter::new();
+    formatter.format_in_file(bindings, &mut f);
 }
 
-pub struct Bindings(
-    SysBindings,
-    HashSet<String>,
-    &'static phf::Map<&'static str, (&'static str, &'static str, &'static str)>,
+pub struct Bindings<'a>(
+    SysBindings<'a>,
+    &'a HashSet<String>,
+    &'a phf::Map<&'static str, (&'static str, &'static str, &'static str)>,
 );
 
-impl Bindings {
+impl<'a> Bindings<'a> {
     #[inline]
-    pub fn linux(bindings: bindgen::Bindings, errors: HashSet<String>) -> Self {
-        Self(SysBindings(bindings), errors, &LINUX_RETURN_VALUES)
+    pub fn linux(bindings: &'a bindgen::Bindings, errors: &'a HashSet<String>) -> Self {
+        Self(SysBindings(bindings, None), errors, &LINUX_RETURN_VALUES)
+    }
+
+    #[inline]
+    pub fn open(bindings: &'a bindgen::Bindings, errors: &'a HashSet<String>) -> Self {
+        Self(
+            SysBindings(bindings, Some((errors, &OPEN_RETURN_VALUES))),
+            errors,
+            &OPEN_RETURN_VALUES,
+        )
     }
 }
 
-impl fmt::Display for Bindings {
+impl<'a> fmt::Display for Bindings<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt::Display::fmt(&self.0, f)?;
-        fmt::Display::fmt(&PamError(self.2, &self.1), f)
+        fmt::Display::fmt(&PamError(self.2, self.1), f)
     }
 }
 
@@ -235,11 +299,9 @@ impl<'a> fmt::Display for PamError<'a> {
         writeln!(f, "#[allow(non_upper_case_globals)]")?;
         writeln!(f, "impl Error {{")?;
 
-        for (&name, &(rname, doc, _)) in self.0 {
+        for (&name, &(rname, doc, err)) in self.0 {
             if name != "PAM_SUCCESS" && self.1.contains(name) {
-                if !doc.is_empty() {
-                    writeln!(f, "/// {}", doc)?;
-                }
+                writeln!(f, "/// {}", if doc.is_empty() { err } else { doc })?;
                 writeln!(f, "pub const {}: Self = Self(self::sys::{});", rname, name)?;
             }
         }
@@ -284,13 +346,35 @@ impl<'a> fmt::Display for PamError<'a> {
     }
 }
 
-pub struct SysBindings(bindgen::Bindings);
+#[allow(clippy::type_complexity)]
+pub struct SysBindings<'a>(
+    &'a bindgen::Bindings,
+    Option<(
+        &'a HashSet<String>,
+        &'a phf::Map<&'static str, (&'static str, &'static str, &'static str)>,
+    )>,
+);
 
-impl fmt::Display for SysBindings {
+impl<'a> fmt::Display for SysBindings<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         writeln!(f, "pub mod sys {{")?;
         fmt::Display::fmt(&SysAllow, f)?;
         fmt::Display::fmt(&self.0, f)?;
+        if let Some((errors, known)) = self.1 {
+            for (&name, _) in known {
+                if errors.contains(name) {
+                    writeln!(f, "pub const {}: i32 = __ORIGINAL_{} as i32;", name, name)?;
+                }
+            }
+        }
+        for name in [
+            "PAM_PROMPT_ECHO_OFF",
+            "PAM_PROMPT_ECHO_ON",
+            "PAM_TEXT_INFO",
+            "PAM_ERROR_MSG",
+        ] {
+            writeln!(f, "pub const {}: i32 = __ORIGINAL_{} as i32;", name, name)?;
+        }
         write!(f, "\n}}")
     }
 }
@@ -429,3 +513,7 @@ impl Default for Formatter {
         Self::new()
     }
 }
+
+pub struct CastSysBindings();
+
+impl CastSysBindings {}
