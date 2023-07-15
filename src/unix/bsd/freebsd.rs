7e0824731a6f545eb7227c4c8044371f768b6c01
shurizzle <shura1991@gmail.com>
2023-07-15 12:20:36 +0200

    freebsd/dragonfly: fix compilation errors

--- src/unix/bsd/freebsd.rs
+++ src/unix/bsd/freebsd.rs
@@ -1,17 +1,13 @@
-use std::{io, rc::Rc};
+use std::io;
 
-use super::{Group, IAMContext, User};
+use tty_info::TtyInfo;
 
 pub(crate) const BOOTTIME_CLOCKID: unix_clock::raw::ClockId = unix_clock::raw::ClockId::Boottime;
 
 #[inline(always)]
 pub(crate) fn process_infos() -> io::Result<(u32, u32, u32, u32, Option<TtyInfo>)> {
-    let tty_info::ProcessInfo {
-        pid,
-        uid,
-        session,
-        tty,
-    } = tty_info::ProcessInfo::current()?;
+    let tty_info::ProcessInfo { pid, session, tty } = tty_info::ProcessInfo::current()?;
+    let uid = unsafe { libc::getuid() };
     let gid = unsafe { libc::getgid() };
 
     Ok((pid, uid, gid, session, tty))
